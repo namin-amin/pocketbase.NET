fd3fbdb0e2c1d21fe7b85239d3821ad3f3fdf992
namin-amin <navaneesh185@gmail.com>
2023-03-03 22:44:20 +0530

    First test

---
+++ test/Mock/MockData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.Mock
+{
+    public static class MockData
+    {
+        public static string testUrl = "http://test.com";
+        public static string testCollName = "posts";
+        public static string testLang = "en-US";   
+    }
+}
---
+++ test/Pocktbase.Test.cs
@@ -0,0 +1,30 @@
+﻿using pocketbase.net;
+using pocketbase.net.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using test.Mock;
+
+namespace test
+{
+    public class PocketbaseTest
+    {
+        public Pocketbase cleint { get; set; } = default!;
+
+        [SetUp]
+        public void Setup()
+        {
+            cleint = new Pocketbase(MockData.testUrl, null, null);
+        }
+
+        [Test]
+        public void TestCleint()
+        {
+            Assert.That(cleint.baseurl, Is.EqualTo(MockData.testUrl));
+            Assert.That(cleint.lang , Is.EqualTo(MockData.testLang));
+            Assert.That(cleint.collection is CollectionService);
+        }
+    }
+}
