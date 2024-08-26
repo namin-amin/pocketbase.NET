85c7d66480b513b12b4bd614c52f69ce7ba52f2d
namin-amin <navaneesh185@gmail.com>
2024-08-26 20:24:42 +0530

    pass failing tesr

--- Tests/Mock/MockData.cs
+++ Tests/Mock/MockData.cs
@@ -8,7 +8,7 @@ namespace Tests.Mock
 {
     public static class MockData
     {
-        public const string testUrl = "http://test.com";
+        public const string testUrl = "http://test.com/";
         public const string testCollName = "posts";
         public const string testLang = "en-US";
     }
